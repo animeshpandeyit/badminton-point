5c92ba6d6b3433a94ceab564cffbd65171ecc0c6
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2025-02-23 01:52:38 +0530

    submitMatchPoint

--- badminton-point/src/app/app.component.ts
+++ badminton-point/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'badminton-point';
+
+  matchpointform: FormGroup;
+
+  matchpointinput: number = 0;
+
+  isDisabled: boolean = false;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.matchpointform = this._formBuilder.group({
+      matchpoint: [''],
+    });
+  }
+  ngOnInit(): void {}
+
+  submitMatchPoint() {
+    this.matchpointinput = this.matchpointform.value.matchpoint; // Extract value
+    this.matchpointform.reset();
+    // this.isDisabled = true; // Disable input after submission
+    this.matchpointform.get('matchpoint')?.disable(); // D
+  }
+  // resetInput() {
+  //   this.matchpointform.get('matchpoint')?.enable(); // Re-enable input field
+  //   this.matchpointform.reset();
+  // }
 }
